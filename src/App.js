e3465f1e0a34306fb215ade76efc4786c0ba0188
rodrigorivas190 <114320458+rodrigorivas190@users.noreply.github.com>
2023-03-24 18:39:46 -0300

    graba primera pre entrega

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import'./App.css';
+import Navbar from "./components/NavBar/NavBar";
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
 function App() {
   return (
-    
-       <> 
-       HOLA MUNDO!
-       </>
-
-      
+    <div className="App">
+      <Navbar />
+      <ItemListContainer greeting={"Bienvenidos"}/>
+    </div>
   
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+
+        <nav>
+            <h3>
+               Lea Liberias 
+            </h3>
+            <div>
+                <button>Romance</button>
+                <button>Ficcion</button>
+                <button>Terror</button>
+                <button>Fantacia</button>
+            </div>
+
+
+        </nav>
+    )
+}
+export default Navbar
